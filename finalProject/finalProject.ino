e38d6571863418234599882b2ca5908fa4412918
carterfodge <carterfodge99@gmail.com>
2018-11-07 01:20:03 -0500

    deleted initial comments

--- finalProject/finalProject.ino
+++ finalProject/finalProject.ino
@@ -1,10 +1,8 @@
 //Visual Drum Sequencer
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
